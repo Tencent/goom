17da0ebc2358322580fd7e4276232ef07b9a166b
yongfuchen <yongfuchen@tencent.com>
2023-11-22 19:01:40 +0800

    feat: 代码开源规范

--- mocker_comp_amd64_test.go
+++ mocker_comp_amd64_test.go
@@ -2,15 +2,6 @@
 // 当前文件实现了对 mocker_test.go,iface_test.go,debug_test.go 的单测对于不同 go 版本的兼容性测试
 package mocker_test
 
-import (
-	"fmt"
-	"os"
-	"strings"
-	"testing"
-
-	"github.com/Jakegogo/goom_mocker/test"
-)
-
 var versions = []string{
 	"go1.13",
 	"go1.14",
@@ -22,31 +13,3 @@ var versions = []string{
 }
 
 const testEnv = "MOCKER_COMPATIBILITY_TEST"
-
-// TestCompatibility 测试针对不同 go 版本的兼容情况
-func TestCompatibility(t *testing.T) {
-	if os.Getenv(testEnv) == "true" {
-		return
-	}
-
-	os.Setenv(testEnv, "true")
-	for _, v := range versions {
-		fmt.Printf("> [%s] start testing..\n", v)
-		if err := test.Run(v, nil, "version"); err != nil {
-			t.Errorf("[%s] env prepare fail: %v", v, err)
-		}
-
-		logHandler := func(log string) {
-			if strings.Contains(log, "--- FAIL:") {
-				t.Errorf("[%s] run fail: see details in the log above.", v)
-			}
-		}
-		if err := test.Run(v, logHandler, "test", "-v", "-gcflags=all=-l", "."); err != nil {
-			t.Errorf("[%s] run error: %v, see details in the log above.", v, err)
-		}
-		if t.Failed() {
-			break
-		}
-		t.Logf("[%s] testing success.", v)
-	}
-}
